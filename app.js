96030ca0cf12adf8ee9ba1227778766e67898e55
kolechits <pavelkolechits@gmail.com>
2023-04-18 23:30:32 +0300

    add food

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const salaryCalc = (hours) => {
+    const dogsFodPrise = 14;
     let temp;
     if(hours > 180){
         temp = (hours - 180) * 1.5
-        return Math.floor((temp + 180) * 8.7)
+        return [Math.floor((temp + 180) * 8.7), Math.floor((temp + 180) * 8.7)/dogsFodPrise]
     }
-    return Math.floor(hours * 8.7)
+    return [Math.floor(hours * 8.7), Math.floor(hours * 8.7)/dogsFodPrise]
 }
 let modal = document.querySelector('.modal');
 let btnOk = document.querySelector('.ok');
 let input = document.querySelector('input');
 let btnCount = document.querySelector('.count')
 let salary = document.querySelector('.salary')
+let fod = document.querySelector('.fod')
 modal.classList.add('hidden')
+
 const actions = () => {
     let hours = input.value
-    salary.innerHTML = `${salaryCalc(hours)} BYN`
+    const [h, kg] = salaryCalc(hours)
+    salary.innerHTML = `${h} BYN`
+    fod.innerHTML = `${kg} kg`
     input.classList.add('hidden')
     modal.classList.remove('hidden')
     btnCount.classList.add('hidden')
 }
+
 btnCount.addEventListener('click', actions)
 btnOk.addEventListener('click', () => {
     input.classList.remove('hidden')
     modal.classList.add('hidden')
     btnCount.classList.remove('hidden')
 })
+
+
